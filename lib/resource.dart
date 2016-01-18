81654f484f9a03a5252d656477d34497a10772e1
Lasse R.H. Nielsen <lrn@google.com>
2016-01-18 13:24:33 +0100

    Add examples to resource package.
    R=floitsch@google.com
    
    Review URL: https://codereview.chromium.org//1606473002 .

--- lib/resource.dart
+++ lib/resource.dart
@@ -2,9 +2,27 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-/// A [Resource] is data that can be loaded into a Dart program.
+/// A [Resource] is data that can be read into a Dart program.
 ///
-/// A resource is identified by a URI.
+/// A resource is identified by a URI. It can be loaded as bytes or data.
+/// The resource URI may be a `package:` URI.
+///
+/// Example:
+///
+///     var resource = new Resource("package:foo/foo_data.txt");
+///     var string = await resource.readAsString(UTF8);
+///     print(string);
+///
+/// Example:
+///
+///     var resource = new Resource("http://example.com/data.json");
+///     var obj = await resource.openRead()   // Reads as stream of bytes.
+///                             .transform(UTF8.fuse(JSON).decoder)
+///                             .first;
+///
+///
+/// Notice: Currently this package requires `dart:io` to do the reading,
+/// so it doesn't work in the browser.
 library resource;
 
 export "src/resource.dart" show Resource;
